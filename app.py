3e8ce4f620bd61c494166a333741b58fd27cb61d
Haripriya-p1702 <haripriyasvb2018@gmail.com>
2026-04-03 15:29:05 +0530

    Code chane

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return "Welcome to Athiva Hackathon'26!"
 
 if __name__ == "__main__":
-            app.run(host="0.0.0.0", port=80000000000)
+            app.run(host="0.0.0.0", port=80000030000)
